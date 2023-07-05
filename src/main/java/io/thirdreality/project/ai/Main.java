d627a63bb132093965b250178cd35f06061a878c
ThirdRealityStudios <janek.piekarski.postfach@gmail.com>
2023-07-05 11:29:45 +0200

    Added little chat bot but it still lacks of depth in the answers, so this is what I will implement next.

--- src/main/java/io/thirdreality/project/ai/Main.java
+++ src/main/java/io/thirdreality/project/ai/Main.java
@@ -1,11 +1,50 @@
 package io.thirdreality.project.ai;
 
+import io.thirdreality.project.ai.core.AI;
+import io.thirdreality.project.ai.core.Equalable;
+import io.thirdreality.project.ai.neuron.ConsoleNeuron;
+import io.thirdreality.project.ai.neuron.Neuron;
+
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main
 {
     public static void main(String[] args)
     {
+        Equalable<String> e = new Equalable<>()
+        {
+            @Override
+            public boolean equals(String o0, String o1)
+            {
+                return o0.equals(o1);
+            }
+        };
 
+        AI<String> ai = new AI<>(e);
+
+        String input = null;
+
+        Scanner s = new Scanner(System.in);
+
+        do
+        {
+            System.out.print("Deine Eingabe: ");
+
+            input = s.nextLine();
+
+            System.out.println();
+
+            ai.synchronize(new ConsoleNeuron(input), 1);
+            ai.finish();
+
+            String n = ai.fire(input);
+
+            System.out.println("AI: " + n + "\n");
+        }
+        while(!"/exit".equals(input));
+
+        s.close();
     }
 }
